d835602bb456e808739588781fb4449d48a5be1f
Mike Robards <mikerobards@icloud.com>
2021-05-07 14:51:34 -0400

    finish lesson

--- fetch-recap/index.js
+++ fetch-recap/index.js
@@ -1,15 +1,29 @@
 
 async function getPost() {
-    const postPromise = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    const [postPromise, userPromise] = await Promise.all([
+        fetch('https://jsonplaceholder.typicode.com/posts'),
+        fetch('https://jsonplaceholder.typicode.com/users')
+    ]) 
+    
+    
 
     if (postPromise.ok) {
         const posts = await postPromise.json()
+        const users = await userPromise.json()
+
+        console.log(users[0])
+
         let html = ''
-        const title = posts.title
-        const body = posts.body
+        const title = posts[0].title
+        const user = users.filter(user => user.id === posts[0].userId)[0].name
+        const company = users.filter(user => user.id === posts[0].userId)[0].company.name
+        const body = posts[0].body
+
         html += `
         <div>
             <h3>${title}</h3>
+            <h5>${user}</h5>
+            <p>${company}</p>
             <p>${body}</p>
         </div>
         `
